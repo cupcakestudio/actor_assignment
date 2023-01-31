389db33b2a549292a34eaba5e6c57218766022d0
Ting Hua Lin <coolting.lin3@gmail.com>
2023-01-31 19:16:47 +0100

    add popop window details, actor list clickable

--- script.js
+++ script.js
@@ -22,8 +22,19 @@ function showActorList(){
 const sectionActor = document.querySelector("#actorListView");
 const temp = document.querySelector("template");
 actors.forEach(actor => {
-  temp.cloneNode(true).content;
+  const actorCloneList = temp.cloneNode(true).content;
+  //add name of actor to clone Node
+  actorCloneList.querySelector("li").textContent = actor.fullname
+  actorCloneList.querySelector("li").addEventListener("click", () => { 
   
+
+  //popop window
+  const popwindow = document.querySelector("#popop");//from babushka exercise in 2.sem
+  document.getElementById("popop").style.display = "block";
+  popwindow.querySelector(".name").textContent = actor.fullname;
+  popwindow.querySelector(".movie").textContent = actor.movie;})
+  //add the actor to the list section
+  sectionActor.appendChild(actorCloneList);
 });
 
 }
